b2c1fb2b0206ddae4fbf6a94a23d1555531690aa
Lex <aligon17@gmail.com>
2021-08-22 14:32:03 -0400

    updated auth.js for req as object in auth middleware

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,8 +6,9 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
+    // possibly add req.body.token????
     let token = req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
@@ -16,7 +17,6 @@ module.exports = {
     }
 
     if (!token) {
-      // return res.status(400).json({ message: 'You have no token!' });
       return req;
     }
 
@@ -26,11 +26,8 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    // next();
     return req;
   },
   signToken: function ({ username, email, _id }) {
